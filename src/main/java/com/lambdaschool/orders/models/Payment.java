7f746b8cc0b91d3c98bb4be85fcc812b74dd99b9
Your Name <you@example.com>
2021-02-16 20:04:15 -0600

    4 - Models Fixed, on to other files

--- src/main/java/com/lambdaschool/orders/models/Payment.java
+++ src/main/java/com/lambdaschool/orders/models/Payment.java
@@ -1,6 +1,9 @@
 package com.lambdaschool.orders.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.util.HashSet;
 
 @Entity
 @Table(name = "payments")
@@ -14,4 +17,25 @@ public class Payment
         unique = true)
     private String type;
 
+    @ManyToMany(mappedBy = "payments")
+    @JsonIgnoreProperties("payments")
+    private Set<Order> orders = new HashSet<>();
+
+    public Payment()
+    {
+        ///jpa
+    }
+
+    public Payment(String type)
+    {
+        this.type = type;
+    }
+
+    public Set<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(Set<Order> orders) {
+        this.orders = orders;
+    }
 }
